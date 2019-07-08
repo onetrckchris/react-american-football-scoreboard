c7034e8e44066ea80e304b50af06acd52024f569
onetrckchris <onetrckchris@gmail.com>
2019-07-08 12:55:56 -0700

    added button to change the quarter

--- src/App.js
+++ src/App.js
@@ -7,13 +7,22 @@ function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
   const [LionsScore, setLionsScore] = useState(0);
   const [TigersScore, setTigersScore] = useState(0);
+  const [GameQuarter, setGameQuarter] = useState(1);
+
+  const changeQuarter = () => {
+    if(GameQuarter < 4) {
+      setGameQuarter(GameQuarter + 1);
+    } else {
+      setGameQuarter(1);
+    }
+  }
 
   return (
     <div className="container">
       <section className="scoreboard">
         <div className="topRow">
           <div className="home">
-            <h2 className="home__name">Lions</h2>
+            <h2 className="home__name">Home</h2>
 
             {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
 
@@ -21,11 +30,11 @@ function App() {
           </div>
           <div className="timer">00:03</div>
           <div className="away">
-            <h2 className="away__name">Tigers</h2>
+            <h2 className="away__name">Guest</h2>
             <div className="away__score">{TigersScore}</div>
           </div>
         </div>
-        <BottomRow />
+        <BottomRow gameQuarter={GameQuarter}/>
       </section>
       <section className="buttons">
         <div className="homeButtons">
@@ -37,6 +46,7 @@ function App() {
           <button className="awayButtons__touchdown" onClick={() => setTigersScore(TigersScore + 6)}>Away Touchdown</button>
           <button className="awayButtons__fieldGoal" onClick={() => setTigersScore(TigersScore + 3)}>Away Field Goal</button>
         </div>
+        <button className="changeQuarter" onClick={changeQuarter}>Change Quarter</button>
       </section>
     </div>
   );
